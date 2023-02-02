55d069dadcc1a627d702c18bed0bd0add4d6bac6
Kae Sluder <kaesluder@fastmail.com>
2023-02-01 19:29:12 -0500

    Default sort and listing tags.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,25 @@
 import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import * as R from 'ramda';
+
+import { DateTime } from 'luxon';
 
 const Item = function (props) {
+  const convertParsedDate = (parsedDate) =>
+    DateTime.fromSeconds(parsedDate).toLocaleString();
+
+  const tagList = props.itemData['filter_results'].map((result) => {
+    return <Chip label={result['tag']} key={result['tag']}></Chip>;
+  });
+
   return (
     <Paper sx={{ m: 1 }}>
       <Accordion>
@@ -15,7 +28,17 @@ const Item = function (props) {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>{props.itemData['title'] ?? 'no title'}</Typography>
+          <Stack>
+            <div>
+              <Typography>{props.itemData['title'] ?? 'no title'}</Typography>
+            </div>
+            <div>
+              <Chip
+                label={convertParsedDate(props.itemData['published_parsed'])}
+              ></Chip>
+              {tagList}
+            </div>
+          </Stack>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>{props.itemData['summary'] ?? 'no summary'}</Typography>
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,8 +1,11 @@
 import Box from '@mui/material/Box';
 import Item from './Item';
+import * as R from 'ramda';
+
+const sortByDate = R.compose(R.reverse, R.sortBy(R.prop('published_parsed')));
 
 const ItemList = function (props) {
-  const renderedList = props.itemsStatus.map((item) => {
+  const renderedList = sortByDate(props.itemsStatus).map((item) => {
     return <Item itemData={item} key={item.id}></Item>;
   });
 
