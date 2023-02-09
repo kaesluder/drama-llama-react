0a9cad98bd59c9a86156488bdd6e33a70d8b6f9f
Kae Sluder <kaesluder@fastmail.com>
2023-02-08 20:22:23 -0500

    adding an event listener to open links in browser

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,10 +1,21 @@
 import Box from '@mui/material/Box';
 import Item from './Item';
 import * as R from 'ramda';
+import { useEffect } from 'react';
 
 const sortByDate = R.compose(R.reverse, R.sortBy(R.prop('published_parsed')));
 
 const ItemList = function (props) {
+  useEffect(() => {
+    /* Unorthodox for React, but ensure that clicked links open 
+  in a browser window rather than the app window. */
+    document.addEventListener('click', function (e) {
+      if (e.target.tagName === 'A' && !e.target.hasAttribute('target')) {
+        e.target.setAttribute('target', '_blank');
+      }
+    });
+  }, []);
+
   const renderedList = sortByDate(props.itemsStatus).map((item) => {
     return <Item itemData={item} key={item.id}></Item>;
   });
