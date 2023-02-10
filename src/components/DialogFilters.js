008790335eda9e4d72fa1901ebe6f1888eb00c0d
Kae Sluder <kaesluder@fastmail.com>
2023-02-10 16:23:23 -0500

    form validation and docstrings

--- src/components/DialogFilters.js
+++ src/components/DialogFilters.js
@@ -30,7 +30,14 @@ export default function DialogFilters(props) {
   };
 
   const [errorState, setErrorState] = useState({});
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+  const [formData, setFormData] = useState(kDefaultFields);
 
+  /**
+   * Returns true if regStr is a valid regular expression.
+   * @param {string} regStr
+   * @returns
+   */
   const validateRegex = function (regStr) {
     try {
       new RegExp(regStr);
@@ -40,6 +47,24 @@ export default function DialogFilters(props) {
     }
   };
 
+  /**
+   * Check that all required values are present before
+   * trying to submit.
+   * @returns
+   */
+  const preSubmitValidate = function () {
+    for (let field in validateFields) {
+      if (!validateFields[field](formData[field])) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  /**
+   * Object for connecting field-names to validation functions.
+   * Each validation function should return a bool.
+   */
   const validateFields = {
     type: (v) => R.find(R.equals(v))(['RegexFilter', 'BaseFilter']),
     id: (v) => v.length > 3,
@@ -47,6 +72,9 @@ export default function DialogFilters(props) {
     regex: (v) => validateRegex(v),
   };
 
+  /**
+   * Object for storing helper texts for each field.
+   */
   const helperTexts = {
     type: 'Only RegexFilter and BaseFilter are supported.',
     id: 'ID must be longer than 3 characters.',
@@ -54,11 +82,13 @@ export default function DialogFilters(props) {
     regex: 'Must be a valid regular expression.',
   };
 
-  const [submitDisabled, setSubmitDisabled] = useState(true);
-
-  const [formData, setFormData] = useState(kDefaultFields);
-
-  const handleListItemClick = (event, index) => {
+  /**
+   * Handles selection of list items from the menu. Changes
+   * form values to data from filter spec in filterList.
+   * @param {event} event
+   * @param {string} index
+   */
+  const handleListItemClick = function (event, index) {
     if (selectedIndex === index) {
       setSelectedIndex(null);
       setFormData(kDefaultFields);
@@ -75,6 +105,9 @@ export default function DialogFilters(props) {
     }
   };
 
+  /** render the list items if filterList has been passed
+   * from the database.
+   */
   let listItems = null;
 
   if (props.filterList) {
@@ -93,6 +126,11 @@ export default function DialogFilters(props) {
     });
   }
 
+  /**
+   * Builds a single input field using fieldName.
+   * @param {fieldName} fieldName
+   * @returns
+   */
   const buildField = function (fieldName) {
     return (
       <div key={fieldName}>
@@ -111,6 +149,9 @@ export default function DialogFilters(props) {
     );
   };
 
+  /** TODO consolidate this with the other validation
+   * function.
+   */
   const missingFormData = function () {
     for (let key in validateFields) {
       if (!validateFields[key](formData[key])) {
@@ -121,6 +162,10 @@ export default function DialogFilters(props) {
     return false;
   };
 
+  /**
+   * Handles change events in text fields.
+   * @param {event} event
+   */
   const handleChange = function (event) {
     const id = event.target.id;
     const value = event.target.value;
@@ -140,14 +185,24 @@ export default function DialogFilters(props) {
     console.log(missingFormData());
   };
 
+  /**
+   * Reset form fields when cancel is selected.
+   * @param {Event} event
+   */
   const handleCancel = function (event) {
     setFormData(kDefaultFields);
     props.toggleFilterDialogOpen();
   };
 
+  /**
+   * Pass formData up to create a filter.
+   * @param {event} event
+   */
   const handleSubmit = function (event) {
-    props.handleAddFilter(formData);
-    handleCancel();
+    if (preSubmitValidate()) {
+      props.handleAddFilter(formData);
+      handleCancel();
+    }
   };
 
   const formFields = R.map(buildField)(R.keys(formData));
